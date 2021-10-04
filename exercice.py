67c5116900ad4736dc0d228869c8d25b59f99605
Maxime Pageot <maxime.pageot.313@gmail.com>
2021-10-04 13:51:32 -0400

    tous les exos finis en 1 lignes chacun.

--- exercice.py
+++ exercice.py
@@ -6,27 +6,21 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
+    return {a:some_list.index(a) for a in some_list}
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    return [(c,cnames[c]) for c in colors]
 
 
 def create_list() -> list:
-    # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
-
-    return []
+    return [n for n in range(10000) if n < 15 or n > 350]
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
-
-    return {}
-
+    return{modele:sum([(n[0]-n[1])**2 for n in model_dict[modele]])/len(model_dict[modele]) for modele in model_dict.keys()}
 
 def main() -> None:
     some_list = ["a", "b", "z", "patate"]
